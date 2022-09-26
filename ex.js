ab0e297d9f6bf0fa4edf37fc6dd880741536aa68
PavelKhmyz <pavlik.schavlik@outlook.com>
2022-09-26 17:32:20 +0300

    hw6.01

--- ex.js
+++ ex.js
@@ -1,9 +1,7 @@
 document
         .querySelector('#but')
         .addEventListener('click', add);
-    
-let span;
-let image;
+
 function add(){
     const tam = document.createElement('div');
     tam.className = 'tam';
@@ -13,79 +11,71 @@ function add(){
     disp.className = 'disp';
     tam.append(disp);
 
-    span = document.createElement('span')
-    span.className = 'span';
-    span.innerHTML = `HP: ${hel}`
-    disp.append(span)
-
-    image = document.createElement('img')
-    image.className = 'dino'
-    image.src = 'dino.jpg'
-    disp.append(image)
-    
     const feed = document.createElement('button');
     feed.className = 'feedButton';
     feed.innerHTML = 'Дать пива';
     tam.append(feed);
 
-    document
-        .querySelector('.feedButton')
-        .addEventListener('click', bla);
+    const image = document.createElement('img');
+    image.className = 'dino';
+    image.src = 'dino.jpg';
+    disp.append(image);
 
-    function bla(){
-        hel = hel + Math.round(hel * 0.2);0
-        if (hel > 100){
-            hel = 101;
-        }
+    function health(){
+        let hel = 100;
+        const span = document.createElement('span');
+        span.className = 'span';
+        span.innerHTML = `HP: ${hel}`;
+        image.before(span);
+
+        feed.addEventListener('click', bla);
+
+        function bla(){
+            hel = hel + Math.round(hel * 0.2);
+            if (hel > 100){
+                hel = 100;
+            };
+            span.innerHTML = `HP: ${hel}`;
+        };
+
+        return  function (){
+            if (hel > 0){
+                hel = hel - 1;
+                span.innerHTML = `HP: ${hel}`;
+            }
+            else{
+                span.innerHTML = 'DEAD';
+                clearInterval(firstInterval);
+                clearInterval(secondInterval);
+                image.style.transform = '';
+                image.className = 'dinoDead';
+            };
+        };             
     };
+    const firstInterval = setInterval(health(), 1000);
+
+    let margin = 0;
+    let direction = true;
+
+    function autoMove(){   
+        if(direction === true){
+            margin = margin + 1;
+            image.style.marginLeft = `${margin}px`;
+            direction = true;
+            if(margin === 130){
+                image.style.transform = 'scale(-1,1)';
+                direction = false;
+            };
+        }
+        else{
+            margin = margin - 1;
+            image.style.marginLeft = `${margin}px`;
+            direction = false;
+            if(margin === 0){
+                image.style.transform = '';
+                direction = true;
+            };
+        };
+    };
+    const secondInterval = setInterval(autoMove, 30); 
 };
-let hel = 100;
-let margin = 0;
-let direction = true;
-
-function move(){
-    margin = margin + 1;
-    document.querySelector('.dino').style.marginLeft = `${margin}px`
-    direction = true;
-    if(margin === 130){
-        document.querySelector('.dino').style.transform = 'scale(-1,1)'
-        direction = false;
-    }
-}
-
-function moveBack(){
-    margin = margin - 1;
-    document.querySelector('.dino').style.marginLeft = `${margin}px`
-    direction = false;
-    if(margin === 0){
-        document.querySelector('.dino').style.transform = ''
-        direction = true;
-    }
-}
-
-function autoMove(){
-    if(direction === true){
-        move();
-    }
-    else{
-        moveBack();
-    }
-}
-
-const secondInterval = setInterval(autoMove, 30);
-
-function health(){
-    if (hel > 0){
-    hel = hel - 1;    
-    span.innerHTML = `HP: ${hel}`
-    }
-    else{
-        span.innerHTML = 'DEAD'
-        clearInterval(intervalId);
-        clearInterval(secondInterval);
-        document.querySelector('.dino').style.transform = ''
-        image.className = 'dinoDead'
-    }    
-}
-const intervalId = setInterval(health, 1000);
-
